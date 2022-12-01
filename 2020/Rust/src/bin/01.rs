f19f2a1255e29f6dbc4a76636217051afb65eb55
Baptiste Cottier <42634014+baptistecottier@users.noreply.github.com>
2022-12-01 22:24:28 +0100

    2020 - Rust - Day 01

--- 2020/Rust/src/bin/01.rs
+++ 2020/Rust/src/bin/01.rs
@@ -1,69 +1,24 @@
-aoc2016::main!();
+aoc2020::main!();
 
-fn generator(input : &str) -> Vec<Vec<i16>> {
-    let mut position = vec![0,0] ; 
-    let mut direction = (0,1) ;
-    input
-        .split(", ")
-        .map(|i| {
-            let mut chars = i.chars() ;
-            (chars.next().unwrap() , chars.as_str().parse::<u16>().unwrap())
-        })
-        .map(|(turn, d)| match turn {
-            'R' => { direction = turn_right(direction);
-                    (direction , d)},
-            'L' => { direction = turn_left(direction);
-                    (direction , d)},
-            _ => unreachable!()})
-        .map(move |((x ,y), l)| (0..l)
-                            .map(|_| 
-                                {
-                                position[0] += x ;
-                                position[1] += y ;
-                                position.clone()
-                                }
-                            )
-                            .collect_vec())
-        .flatten()
+fn generator(input : &str) -> Vec<u32> {
+   input
+        .lines()
+        .map(|l| l.parse().unwrap())
         .collect_vec()
 }
 
-fn part_1(input : Vec<Vec<i16>> ) -> i16 {
-    input
-        .iter()
-        .next_back()
+fn part_1(input : Vec<u32> ) -> u32 {
+    iproduct!(input.clone(), input.clone())
+        .into_iter()
+        .find(|(a,b)| a + b == 2020)
+        .map(|(a,b)| a * b)
         .unwrap()
-        .iter()
-        .map(|x| x.abs())
-        .sum()
-    }
-
-fn part_2(input : Vec<Vec<i16>> ) -> i16 {
-   input
-        .iter()
-        .find(|pos| input.clone().iter().filter(|x| x==pos).count() > 1)
-        .unwrap()
-        .iter()
-        .map(|x| x.abs())
-        .sum()
-    }
-
-fn turn_left(input : (i16, i16)) -> (i16, i16) {
-    match input {
-        (-1,0) => (0,-1),
-        (0,1)  => (-1,0),
-        (1,0)  => (0,1),
-        (0,-1) => (1,0),
-        _ => unreachable!()
-    }
 }
 
-fn turn_right(input : (i16, i16)) -> (i16, i16) {
-    match input {
-        (-1,0) => (0,1),
-        (0,1)  => (1,0),
-        (1,0)  => (0,-1),
-        (0,-1) => (-1,0),
-        _ => unreachable!()
-    }
+fn part_2(input : Vec<u32> ) -> u32 {
+    iproduct!(input.clone(), input.clone(), input.clone())
+        .into_iter()
+        .find(|(a,b,c)| a + b + c == 2020)
+        .map(|(a,b,c)| a * b * c)
+        .unwrap()
 }
