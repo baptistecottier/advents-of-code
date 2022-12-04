0a421d37d8bc018726fc8602fc150a970154db2b
Baptiste Cottier <42634014+baptistecottier@users.noreply.github.com>
2022-12-04 20:38:06 +0100

    2022 - Python - Day 03 : Better indexing

--- 2022/Python/03.py
+++ 2022/Python/03.py
@@ -8,4 +8,4 @@ def part_2(input) :
     return solver(input, 3)
 
 def solver(input, size) : 
-    return sum(ord([x for x in input[size * i : size * (i + 1)][0][: (size ** 2 % 7) * len(input[size * i : size * (i + 1)][0]) // 2:] if all([x in g[  size % 3 * len(g) //2 :] for g in input[size * i : size * (i + 1)][:size]])][0]) - 38 - [x for x in input[size * i : size * (i + 1)][0][: (size ** 2 % 7) * len(input[size * i : size * (i + 1)][0]) // 2:] if all([x in g[  size % 3 * len(g) //2 :] for g in input[size * i : size * (i + 1)][:size]])][0].islower() * 58 for i in range(len(input) // size))
+    return sum(ord([x for x in input[size * i : size * (i + 1)][0][:- (size % 3) * len(input[size * i : size * (i + 1)][0]) // 2:] if all([x in g[  size % 3 * len(g) //2 :] for g in input[size * i : size * (i + 1)][:size]])][0]) - 38 - [x for x in input[size * i : size * (i + 1)][0][: (size ** 2 % 7) * len(input[size * i : size * (i + 1)][0]) // 2:] if all([x in g[  size % 3 * len(g) //2 :] for g in input[size * i : size * (i + 1)][:size]])][0].islower() * 58 for i in range(len(input) // size))
