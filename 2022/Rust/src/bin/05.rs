01f7a006e047f498bfe818ec0a2e2439866220f6
Baptiste Cottier <42634014+baptistecottier@users.noreply.github.com>
2022-12-05 10:48:02 +0100

    2022 - Rust - Day 05 : Cleaning

--- 2022/Rust/src/bin/05.rs
+++ 2022/Rust/src/bin/05.rs
@@ -1,52 +1,47 @@
 aoc2022::main!(); 
 
 fn generator(input : &str) -> (Vec<String> , Vec<(usize, usize, usize)>) {
-    let (a, r) = input
-        .split("\n\n")
-        .collect_tuple().unwrap() ; 
+    let (crates, rearrangements) = input.split("\n\n").collect_tuple().unwrap() ; 
     
-    let rearrangements = 
-        r
+    let mut cargo = vec!["".to_string() ; crates.chars().nth_back(1).unwrap().to_digit(10).unwrap() as usize] ;
+        crates
             .lines()
-            .map(|r| {
-                let rr =r.split_whitespace().collect_vec() ;
-                (rr[1].parse::<usize>().unwrap(), rr[3].parse::<usize>().unwrap(), rr[5].parse::<usize>().unwrap())  
-            })
-            .collect_vec() ;
-    let cargo_width : usize = a.trim_end().rsplit_once(' ').unwrap().1.parse().unwrap() ;
-    let mut cargo = vec!["".to_string() ; cargo_width] ;
-        a
-            .lines()
-            .for_each(| c | {
+            .for_each( |c| {
                 let v = c.split('[').collect_vec() ; 
                 let mut s = v[0].len() / 4 ;
                 (v[1..])
-                    .iter()
-                    .for_each(|cc| {
-                        cargo[s].push(cc.chars().nth(0).unwrap()) ;
-                        s += 1 + (cc.chars().filter(|ch| ch.is_whitespace()).count() / 4) ;
-                    })
-            }) ;
+                .iter()
+                .for_each( |cc| {
+                    cargo[s].push(cc.chars().nth(0).unwrap()) ;
+                    s += 1 + cc.chars().filter(|ch| ch.is_whitespace()).count() / 4 ;
+                })
+            });
 
-    (cargo , rearrangements)
+    (cargo , rearrangements
+                .lines()
+                .map(|r| r.split_whitespace().collect_vec().iter().filter_map(|s| s.parse().ok()).collect_tuple().unwrap())
+                .collect_vec())
 }
 
 fn part_1(input : (Vec<String> , Vec<(usize, usize, usize)>)) -> String {
-    solver(input, true)
+    solver(input, 3000)
 }
 
 fn part_2(input : (Vec<String> , Vec<(usize, usize, usize)>)) -> String {
-    solver(input, false)
+    solver(input, 3001)
 }
 
-fn solver(input : (Vec<String> , Vec<(usize, usize, usize)>) , old_version : bool) -> String {
+fn solver(input : (Vec<String> , Vec<(usize, usize, usize)>) , old_version : u16) -> String {
     let (mut cargo, rearrangements) = input ; 
     rearrangements
         .iter()
         .for_each(
             |(l,s,e)| {
-                let mut to_push : String = cargo[s-1].drain(..l).collect() ;
-                if old_version {to_push = to_push.chars().rev().join("")}
+                let to_push : String = match old_version {
+                    3000 => cargo[s-1].drain(..l).rev().collect() ,
+                    3001 => cargo[s-1].drain(..l).collect() ,
+                    _ => unreachable!()
+                };
                 cargo[e-1].insert_str(0 , to_push.as_str()) ;
             });
     cargo
