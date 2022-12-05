e120df46c8ba6a4a6d480fb9dcaa8791fd0ea1fe
Baptiste Cottier <42634014+baptistecottier@users.noreply.github.com>
2022-12-05 07:59:10 +0100

    2022 - Python - Day 05 : Adding

---
+++ 2022/Python/05.py
@@ -0,0 +1,32 @@
+from parse import *
+
+def generator(input) : 
+    [a, b] = input.split('\n\n')
+    rearrangements = []
+    for r in b.splitlines() :
+        rearrangements.append(parse("move {:d} from {:d} to {:d}", r)[:3])
+    
+    cargo_lines = a.splitlines()
+    cargo_width = int(cargo_lines[-1][-2])
+    cargo = ["" for _ in range(cargo_width)]
+    for c in cargo_lines[:-1] : 
+        v = c.split('[')
+        s = len(v[0]) // 4
+        for cc in v[1:] :
+            cargo[s] =  cc[0] + cargo[s]
+            s += 1 + (cc.count(' ') // 4)
+    return (cargo, rearrangements)
+
+def part_1(input) :
+    return solver(input, -1)
+
+def part_2(input) :
+    return solver(input, 1)
+
+def solver(input, version) : 
+    (cargo , rearrangements) = input
+    cargo = list(cargo)
+    for (l, s, e) in rearrangements :
+        cargo[e-1] += cargo[s-1][-l:][::version]
+        cargo[s-1] = cargo[s-1][:-l]
+    return ''.join([c[-1] for c in cargo])
