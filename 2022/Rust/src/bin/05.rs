047f62bdb8856403050d436c03036af26f7cd6ae
Baptiste Cottier <42634014+baptistecottier@users.noreply.github.com>
2022-12-05 12:29:52 +0100

    2022 - Rust - Day 05 : Use function as argument

--- 2022/Rust/src/bin/05.rs
+++ 2022/Rust/src/bin/05.rs
@@ -3,48 +3,47 @@ aoc2022::main!();
 fn generator(input : &str) -> (Vec<String> , Vec<(usize, usize, usize)>) {
     let (crates, rearrangements) = input.split("\n\n").collect_tuple().unwrap() ; 
     
-    let mut cargo = vec!["".to_string() ; crates.chars().nth_back(1).unwrap().to_digit(10).unwrap() as usize] ;
+    let mut stacks = vec!["".to_string() ; crates.chars().nth_back(1).unwrap().to_digit(10).unwrap() as usize] ;
         crates
             .lines()
-            .for_each( |c| {
-                let v = c.split('[').collect_vec() ; 
-                let mut s = v[0].len() / 4 ;
-                (v[1..])
+            .for_each( |level| {
+                let containers = level.split('[').collect_vec() ; 
+                let mut row = containers[0].len() / 4 ;
+                (containers[1..])
                 .iter()
-                .for_each( |cc| {
-                    cargo[s].push(cc.chars().nth(0).unwrap()) ;
-                    s += 1 + cc.chars().filter(|ch| ch.is_whitespace()).count() / 4 ;
+                .for_each( |container| {
+                    stacks[row].push(container.chars().nth(0).unwrap()) ;
+                    row += 1 + container.chars().filter(|char| char.is_whitespace()).count() / 4 ;
                 })
             });
 
-    (cargo , rearrangements
+    (stacks , rearrangements
                 .lines()
                 .map(|r| r.split_whitespace().collect_vec().iter().filter_map(|s| s.parse().ok()).collect_tuple().unwrap())
+                .map(|(n, start, end)| (n, start-1, end-1))
                 .collect_vec())
 }
 
 fn part_1(input : (Vec<String> , Vec<(usize, usize, usize)>)) -> String {
-    solver(input, 3000)
+    solver(input, |c| c.rev().join(""))
 }
 
 fn part_2(input : (Vec<String> , Vec<(usize, usize, usize)>)) -> String {
-    solver(input, 3001)
+    solver(input, |mut c| c.join(""))
 }
 
-fn solver(input : (Vec<String> , Vec<(usize, usize, usize)>) , old_version : u16) -> String {
-    let (mut cargo, rearrangements) = input ; 
-    rearrangements
+fn solver<F>(input : (Vec<String> , Vec<(usize, usize, usize)>) , f : F) -> String 
+where
+    F: Fn(std::string::Drain) -> String,
+{
+    let (mut stacks, _) = input ; 
+    input.1
         .iter()
-        .for_each(
-            |(l,s,e)| {
-                let to_push : String = match old_version {
-                    3000 => cargo[s-1].drain(..l).rev().collect() ,
-                    3001 => cargo[s-1].drain(..l).collect() ,
-                    _ => unreachable!()
-                };
-                cargo[e-1].insert_str(0 , to_push.as_str()) ;
+        .for_each(|&(n,s,e)| { 
+            let crates : &str = &f(stacks[s].drain(..n));
+            stacks[e].insert_str(0 , crates) ;
             });
-    cargo
+    stacks
         .iter()
         .map(|c| c.chars().nth(0).unwrap())
         .join("")
