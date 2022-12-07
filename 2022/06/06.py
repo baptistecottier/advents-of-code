9a4928b290f9f20b98f5d9919323e309605eeb7d
Baptiste Cottier <42634014+baptistecottier@users.noreply.github.com>
2022-12-07 06:28:13 +0100

    Update 06.py

--- 2022/06/06.py
+++ 2022/06/06.py
@@ -1,10 +1,5 @@
 def generator(input) : 
     return input
-
-def part_1(input) :
-    for i in range(10, 26):
-        print("[[bin]]\nname= \""+str(i)+"\"\npath=\"../../"+str(i)+"/"+str(i)+".rs\"\n\n")
-    return solver(input, 4)
     
 def part_2(input) :
     return solver(input, 14)
