98ddda5db7bf8e6b20445356ae92f8cf56fdad94
Baptiste Cottier <42634014+baptistecottier@users.noreply.github.com>
2022-12-07 22:37:26 +0100

    2022 - 07 - Rust : Cleaner code

--- 2022/07/07.rs
+++ 2022/07/07.rs
@@ -1,51 +1,73 @@
 aoc2022::main!() ;
 
 fn generator(input : &str) -> Vec<&str>{
+    input.lines().collect_vec()
+}
+
+fn part_1(input : Vec<&str>) -> u32{
+    solver(input, 100_000, false).iter().sum()
+}
+
+fn part_2(input : Vec<&str>) -> u32 {
+    let to_free = input
+        .iter()
+        .filter(|cmd| ! (cmd.contains("$") || cmd.contains("dir")))
+        .map(|n| n.split(' ').collect_vec().iter().nth(0).unwrap().parse::<u32>().unwrap())
+        .sum::<u32>() 
+        - 40_000_000 ;
+    *solver(input.clone(), to_free, true).iter().min().unwrap()
+
+}
+
+fn solver (input :Vec<&str>, bound : u32, delete : bool) -> Vec<u32> {
+    let mut path : String = root();
+    let mut sizes : HashMap<String, u32> = HashMap::new();
     input
-        .lines()
-        .collect_vec()
-}
-
-fn part_1(input : Vec<&str>) -> i32{
-    solver(input, 100_000, false)
         .iter()
-        .sum()
-}
-
-fn part_2(input : Vec<&str>) -> i32 {
-    *solver(input.clone(), solver(input.clone(), 0, true).iter().max().unwrap() - 40_000_000, true)
-        .iter()
-        .min()
-        .unwrap()
-}
-
-fn solver (input :Vec<&str>, bound : i32, delete : bool) -> Vec<i32> {
-    let mut path : Vec<String> = ["root".to_string()].to_vec() ;
-    let mut sizes : HashMap<String, i32> = HashMap::new();
-    input
-        .iter()
-        .enumerate()
-        .for_each(|(n,command)| {
+        .for_each(| command | {
             let details = command.split(' ').collect_vec() ;
             match details[0] {
-            "$" => match details[1..].concat().as_str() {
-                "cd.." => path.truncate(&path.len()-1),
-                "cd/"  =>  () ,
-                "ls"    => () ,
-                _       => path.push([details[2] , n.to_string().as_str()].concat().to_string())} 
             "dir"   => (),
-            _       =>  path.iter().for_each(|folder|
-                            if sizes.contains_key(folder) {
-                                sizes.insert(folder.to_string(), details[0].parse::<i32>().unwrap() + sizes[folder]);
-                            } else { sizes.insert(folder.to_string(), details[0].parse::<i32>().unwrap()) ; })
-                }      
+            "$" => match details[1..].concat().as_str() {
+                "ls"    => (),
+                "cd/"   => path = root(),
+                "cd.."  => path = back(path.clone()),
+                _       => path = open(path.clone(), details[2])} 
+            _       =>  sizes = update_sizes(path.clone() , sizes.clone(), details[0])}
             }
         );
-        sizes
+    
+    sizes
         .iter()
         .map(|(folder, _)| sizes[folder])
-        .filter(|n| if delete {n > &bound} else {n <= &bound})
-        .collect_vec()
-        .into()
-    
+        .filter(|&n|   (n > bound) == delete )
+        .collect_vec()    
+}
+
+fn root() -> String {
+    return "root/".to_string()
+}
+
+fn back(path : String) -> String {
+    return [path.rsplitn(3 , '/').collect_vec().iter().nth_back(0).unwrap().to_string(),"/".to_string()].concat()
+}
+
+fn open(path : String, new_folder : &str) -> String {
+    return [path.to_string(),new_folder.to_string(),"/".to_string()].concat()
+}
+
+fn update_sizes(path : String, mut sizes : HashMap<String, u32>, size : &str) -> HashMap<String, u32> {
+    let paths = path.split_terminator('/').collect_vec() ;
+    let file_size = size.parse::<u32>().unwrap() ;
+    paths
+        .iter()
+        .scan("".to_string(), |p , &f| {
+            p.push_str(f) ; 
+            Some(p.clone())})
+        .for_each(|folder| {
+        if sizes.contains_key(&folder) { 
+            sizes.insert(folder.clone(), file_size + sizes[&folder])} else {
+            sizes.insert(folder.clone(), file_size)};
+        }) ;
+    return sizes
 }
