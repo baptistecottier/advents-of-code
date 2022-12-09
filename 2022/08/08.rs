1ca5792f885925bd0b847d4570e409847eda73ad
Baptiste Cottier <42634014+baptistecottier@users.noreply.github.com>
2022-12-09 01:09:40 +0100

    2022 - 08 - Rust : Better parsing

--- 2022/08/08.rs
+++ 2022/08/08.rs
@@ -2,44 +2,23 @@ aoc2022::main!();
 
 fn generator(input : &str) -> Vec<(u32 , Vec<Vec<u32>>)> {
     let sl = input.lines().count() ; 
-    let grid = input.clone()
-        .lines()
-        .map(|l| l.chars().map(|c| c.to_digit(10).unwrap()).collect_vec())
-        .flatten()
-        .collect_vec();
+    let trees = input.chars().filter_map(|c| c.to_string().parse::<u32>().ok()).collect_vec() ; 
     
-    let h_views = grid.clone()
-        .chunks(sl)
-        .map(|r| (0..sl).map(|n| { let mut a = r[0..n].to_vec() ;
-            a.reverse() ; 
-            vec![a , r[n+1..].to_vec()]}).collect_vec())
+    let h_views = trees.chunks(sl)
+        .map(|r| (0..sl).map(move |n| vec![rev_slice(&r[0..n]) , r[n+1..].to_vec()]))
         .flatten()
         .collect_vec() ;
 
-
-    let v_views = grid
-    .iter()
-    .enumerate()
-    .sorted_by_key(|(n,_)| (n % sl))
-    .map(|(_, c)| *c)
-    .collect_vec()
-    .chunks(sl) 
-    .map(|r| (0..sl).map(|n| { let mut a = r[0..n].to_vec() ;
-        a.reverse() ; 
-        vec![a ,  r[n+1..].to_vec()]}).collect_vec())
-    .flatten()
-    .enumerate()
-    .sorted_by_key(|(n,_)| (n % sl))
-    .map(|(_, c)| c)
-    .collect_vec();
-
-    let test = zip(grid.clone(), zip(h_views.clone(), v_views.clone()))
-        .map(|(t , (mut hv , mut vv))| {
-            hv.append(&mut vv);
-            (t, hv) 
-})
-        .collect_vec() ; 
-    test
+    let v_views = (0..sl)
+        .map(|n| (0..sl)
+            .map(|i| trees.chunks(sl).map(|l| l[i].clone()).collect_vec())
+            .map(move |r| vec![rev_slice(&r[0..n]) , r[n+1..].to_vec()]))
+        .flatten()
+        .collect_vec() ;
+  
+    zip(trees, zip(h_views, v_views))
+        .map(|(t , (hv , vv))| (t , [hv, vv].concat()))
+        .collect_vec()
 }
 
 fn part_1(input : Vec<(u32 , Vec<Vec<u32>>)>  ) -> usize {
@@ -53,10 +32,8 @@ fn part_2(input:  Vec<(u32 , Vec<Vec<u32>>)>  ) -> usize {
     input
         .iter()
         .map(|( size , views )| views.iter().fold(1, |score , l | {
-            let mut lview = l.iter().take_while(|t| t < &size).count() ;
-            if lview != l.len() {lview += 1} 
-            score * lview }
-        ))
+            let lview = l.iter().take_while(|t| *t < size).count() ;
+            if lview == l.len() {score * (lview)} else {score * (lview + 1)}}))
         .max()
         .unwrap()
 }
