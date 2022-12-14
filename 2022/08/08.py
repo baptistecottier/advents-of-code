142c3d21c04add9ce615df7c5f843ec7fda9047f
Baptiste Cottier <42634014+baptistecottier@users.noreply.github.com>
2022-12-14 03:00:41 -0500

    2022 - 08 - Python : Use of a solver

--- 2022/08/08.py
+++ 2022/08/08.py
@@ -4,30 +4,26 @@ def generator(input) :
     trees = []
     grid = [[int(item) for item in list(line)] for line in input.splitlines()]
     
-    for y in range(len(grid)):
-        for x in range(len(grid[0])):
-            trees.append([grid[y][x], [grid[y][:x+1][::-1],grid[y][x:],[grid[dy][x] for dy in range(y+1)][::-1] , [grid[dy][x] for dy in range(y,len(grid))]]])
+    for (x,y) in itertools.product(range(len(grid)), repeat = 2):
+        trees.append([grid[y][x], [grid[y][:x+1][::-1],grid[y][x:],[grid[dy][x] for dy in range(y+1)][::-1] , [grid[dy][x] for dy in range(y,len(grid))]]])
     return trees
     
 def part_1(input) : 
-    cnt = 0
-    for [size, views] in input :
-        for view in views :
-            if max(view) ==  size and view.count(size) <= 1 : 
-                cnt += 1
-                break
-    return cnt
+    return sum(solver(input, False))
             
 
 def part_2(input) : 
-    best = 0
+    return max(solver(input, True))
+
+
+def solver(input, find_best_tree) : 
+    out = []
     for [size, views] in input :
         score = 1
-        for view in views :
-            cnt = len(list(itertools.takewhile(lambda t : t < size , view[1:])))
-            if cnt != len(view) -1 : cnt += 1
-            score *= cnt
-        best = max(best, score)
-    return best
-
-
+        if find_best_tree:
+            for view in views :
+                cnt = len(list(itertools.takewhile(lambda t : t < size , view[1:])))
+                score *= (cnt +( (cnt + 1) != len(view)))
+            out.append(score)
+        else : out.append(any([max(view) ==  size and view.count(size) <= 1  for view in views]) )
+    return out
