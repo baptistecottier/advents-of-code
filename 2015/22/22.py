1a5f8ffdd2a090c3076f669ecd32c15958576260
Baptiste Cottier <42634014+baptistecottier@users.noreply.github.com>
2022-12-14 10:46:27 -0500

    2015 - 22 - Python : Working solution

--- 2015/22/22.py
+++ 2015/22/22.py
@@ -8,78 +8,82 @@ def generator(specs) :
         s.append(int(spec.split(': ')[1]))
     return s
 
+
 def part_1(input) : 
-    manas = player_turn([], 10, 250 , 13, 8 , 0 , False)
-    return min(eval('['+str(manas).replace('[','').replace(']','')+']'))
+    list_manas = [10000000]
+    player_turn([], 50 , 500 , 71,10 , 0 , False, list_manas)
+    return min(list_manas)
 
 def part_2(input) : 
-    2
+    list_manas = [10000000]
+    player_turn([], 50 , 500 , 71, 10 , 0 , True, list_manas)
+    return min(list_manas)
+
+
     
-def player_turn(effects, player_hp, player_mana,  boss_hp, boss_damage, spent_mana, hard_mode):
-    print("Player :", player_hp, boss_hp)
+def player_turn(effects_t, player_hp, player_mana,  boss_hp, boss_damage, spent_mana, hard_mode, list_manas):
+    if spent_mana > min(list_manas) :
+        return 
+    effects = copy.deepcopy(effects_t)
     # Damages application
-    player_damage = sum([effect[2][0] for effect in effects])
+    player_damage = sum([effect[2] for effect in effects])
     boss_hp -= player_damage
-    if boss_hp <= 0 : return spent_mana
-    if hard_mode : 
-        player_hp -= hard_mode
+    player_hp -= hard_mode
+    if player_hp <= 0 : return 
+    if boss_hp <= 0 : 
+        list_manas.append(spent_mana)
+        return
     
     # Recharge mana
     player_mana +=  sum([effect[4] for effect in effects])
     
     # Effects Update
     for effect in effects :
-        if effect[1] == 1 : effects.remove(effect)
-        else : effect[1] -= 1
+        effect[1] -= 1
+
+    for effect in effects :
+        if effect[1] == 0 : 
+            effects.remove(effect)
         
      # New cast   
+    if player_mana < 53 : return
     le = [effect[0] for effect in effects]
-    a, b, c, d, e = 5000, 5000, 5000, 5000, 5000
-    if player_mana < 53 : return sys.maxsize
-    a = boss_turn([item for item in effects], player_hp, player_mana - 53,  boss_hp, boss_damage, spent_mana + 53, hard_mode)
-    if player_mana >= 73 : b = boss_turn([item for item in effects], player_hp + 2, player_mana - 73,  boss_hp - 2, boss_damage, spent_mana + 73, hard_mode)
-    if player_mana >= 229 :e = boss_turn([item for item in effects] + [[229 , 5, [0, 0], 0, 101]], player_hp, player_mana - 229,  boss_hp , boss_damage, spent_mana + 229, hard_mode) 
-    if player_mana >= 113 :c = boss_turn([item for item in effects] + [[113 , 6, [0, 0], 7, 0]], player_hp, player_mana - 113,  boss_hp , boss_damage, spent_mana + 113, hard_mode) 
-    if player_mana >= 173 :d = boss_turn([item for item in effects] + [[173, 6, [3, 0], 0, 0]], player_hp, player_mana - 173,  boss_hp , boss_damage, spent_mana + 173, hard_mode) 
-    return min([a,b,c,d,e])
+    if spent_mana > min(list_manas) : return 
+    if player_mana >= 53 : a = boss_turn(effects, player_hp, player_mana - 53,  boss_hp - 4, boss_damage, spent_mana + 53, hard_mode, list_manas)
+    if player_mana >= 73 : b = boss_turn(effects, player_hp + 2, player_mana - 73,  boss_hp - 2, boss_damage, spent_mana + 73, hard_mode, list_manas)
+    if player_mana >= 229 and 229 not in le: e = boss_turn(effects + [[229 , 5, 0, 0, 101]], player_hp, player_mana - 229,  boss_hp , boss_damage, spent_mana + 229, hard_mode, list_manas) 
+    if player_mana >= 113 and 113 not in le: c = boss_turn(effects + [[113 , 6, 0, 7, 0]], player_hp, player_mana - 113,  boss_hp , boss_damage, spent_mana + 113, hard_mode, list_manas) 
+    if player_mana >= 173 and 173 not in le: d = boss_turn(effects + [[173, 6, 3, 0, 0]], player_hp, player_mana - 173,  boss_hp , boss_damage, spent_mana + 173, hard_mode, list_manas) 
 
 
 
 
 
-def boss_turn(effects, player_hp, player_mana,  boss_hp, boss_damage, spent_mana, hard_mode):
-    print("Boss :", player_hp, boss_hp)
-    if player_mana < 0 : return sys.maxsize
-    if spent_mana > 2000 : return 2000
+def boss_turn(effects_t, player_hp, player_mana,  boss_hp, boss_damage, spent_mana, hard_mode, list_manas):
+    effects = copy.deepcopy(effects_t)
+
     # Damage application application
-    player_damage = sum([effect[2][0] for effect in effects])
+    player_damage = sum([effect[2] for effect in effects])
     boss_hp -= player_damage
-    if boss_hp <= 0 : return spent_mana
+    if boss_hp <= 0 :
+        list_manas.append(spent_mana)
+        return 
     
     # Recharge mana
     player_mana +=  sum([effect[4] for effect in effects])
-    
+
     # Boss attack
     player_hp -= max(1, boss_damage - sum([effect[3] for effect in effects]))
-    if player_hp <= 0 : return sys.maxsize
+    if player_hp <= 0 : 
+        return 
     
     # Effect update
-    for effect in effects :
-        if effect[1] == 1 : effects.remove(effect)
-        else : effect[1] -= 1
-        
-    return player_turn([item for item in effects], player_hp, player_mana,  boss_hp, boss_damage, spent_mana, hard_mode)
 
-def bfs(maze, start , end):
-    width , height = len(maze[0]) , len(maze)
-    queue = collections.deque([[start]])
-    seen = set([start])
-    while queue:
-        path = queue.popleft()
-        x, y = path[-1]
-        if (x , y) == end:
-            return len(path)-1
-        for x2, y2 in ((x+1,y), (x-1,y), (x,y+1), (x,y-1)):
-            if 0 <= x2 < width and 0 <= y2 < height and maze[y2][x2] != '#' and (x2, y2) not in seen:
-                queue.append(path + [(x2, y2)])
-                seen.add((x2, y2))
+    for effect in effects :
+        effect[1] -= 1
+
+    for effect in effects :
+        if effect[1] == 0 : 
+            effects.remove(effect)
+        
+    return player_turn(effects, player_hp, player_mana,  boss_hp, boss_damage, spent_mana, hard_mode, list_manas)
