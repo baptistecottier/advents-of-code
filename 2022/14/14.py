81bfbc673ddee0c77b28d08c70dd2aaf6caf44f0
Baptiste Cottier <42634014+baptistecottier@users.noreply.github.com>
2022-12-14 05:49:10 -0500

    2022 - 14 - Rust & Python : minor

--- 2022/14/14.py
+++ 2022/14/14.py
@@ -1,43 +1,42 @@
 def generator(input) : 
     paths = []
+    cave = [[0 for _ in range(1000)] for _ in range(200)]
     for line in input.splitlines() : 
         rock_path = []
         for pos in line.split(' -> ') :
             rock_path.append([int(item) for item in pos.split(',')])
         paths.append(rock_path)
-    grid = [[0 for _ in range(1000)] for _ in range(1000)]
 
     max_depth = 0
     for path in paths : 
-        for i in range(len(path) -1):
-            [xa, ya] , [xb, yb] = path[i] , path[i+1]
-            for y in range(min(ya, yb), max(ya, yb)+1):
+        for [xa, ya] , [xb, yb] in zip(path, path[1:]):
+            for x, y in range(min(ya, yb), max(ya, yb)+1):
                 max_depth = max(y, max_depth)
                 for x in range(min(xa, xb), max(xa, xb)+1):
-                    grid[y][x] = 1
-    return grid, max_depth
+                    cave[y][x] = 1
+                    
+    for x in range(1000) : cave[max_depth + 2][x] = 1
+
+    return cave, max_depth
 
 def part_1(input) : 
-    return solver(input, False)
+    return solver(input[0], input[1], False)
 
 def part_2(input) : 
-    return solver(input, True)
+    return solver(input[0], input[1], True)
             
 
-def solver(input, floor) :
-    grid, max_depth = input
-    if floor : 
-        max_depth += 2
-        for x in range(1000) : grid[max_depth][x] = 1
+def solver(cave, max_depth, floor) :
+    x, y, sand = 500, 0, 0
+    
+    while cave[0][500] == 0 and y < max_depth + 2 * floor :
+        match (cave[y+1][x-1] , cave[y+1][x] , cave[y+1][x+1]) :
+            case (_, 0, _ )  : y += 1
+            case (0, _, _)   : (x , y) = (x - 1, y + 1)
+            case (_, _, 0)   : (x , y) = (x + 1, y + 1)
+            case _           : 
+                cave[y][x] = 1
+                (x, y) = (500, 0)
+                sand += 1
 
-    cnt = 0
-    x, y = 500, 0
-    while True :
-        if grid[0][500] == 1 or y >= max_depth : return cnt
-        if grid[y+1][x] == 0 : y += 1
-        elif all([grid[y+1][i] == 1 for i in [x-1, x + 1]]) :
-            grid[y][x] = 1
-            cnt += 1
-            x, y = 500, 0
-        elif grid[y+1][x-1] == 0 : x , y = x - 1 , y + 1
-        elif grid[y+1][x+1] == 0 : x, y = x + 1, y + 1
+    return sand
--- 2022/14/14.rs
+++ 2022/14/14.rs
@@ -1,6 +1,6 @@
 aoc2022::main!() ;
 
-fn generator(input : &str) -> ([[u32 ; 1000] ; 200] , usize) {
+fn generator(input : &str) -> ([[u8 ; 1000] ; 200] , usize) {
     let mut cave = [[0 ; 1000] ; 200];
     let mut max_depth = 0 ;
     input
@@ -9,38 +9,31 @@ fn generator(input : &str) -> ([[u32 ; 1000] ; 200] , usize) {
         .for_each(|path| path.iter().tuple_windows().for_each(|(start, end)| 
             iproduct!(start[0].min(end[0]) .. start[0].max(end[0]) + 1 , start[1].min(end[1]) .. start[1].max(end[1]) + 1)
                 .for_each(|(x, y)| {
-                max_depth = max_depth.max(y) ; 
-                cave[y][x]=1 ; })
+                    max_depth = max_depth.max(y) ; 
+                    cave[y][x] = 1 ; })
     ));
-    ( cave, max_depth)
+    (0 .. 1000).for_each(|x| cave[max_depth + 2][x] = 1) ;
+    (cave, max_depth)
 }
 
-fn part_1(input :  ([[u32 ; 1000] ; 200] , usize)) -> u32 {
+fn part_1(input :  ([[u8 ; 1000] ; 200] , usize)) -> usize {
     solver(input.0, input.1, false)
 }
 
-fn part_2(input :  ([[u32 ; 1000] ; 200] , usize)) -> u32 {
+fn part_2(input :  ([[u8 ; 1000] ; 200] , usize)) -> usize {
     solver(input.0, input.1 , true)
 }
 
-fn solver(mut cave : [[u32 ; 1000] ; 200], mut max_depth : usize, floor : bool) -> u32 {
+fn solver(mut cave : [[u8 ; 1000] ; 200], max_depth : usize, floor : bool) -> usize {
+    let (mut x, mut y, mut sand) = (500, 0, 0);
 
-    let mut sand = 0 ; 
-    let (mut x, mut y) = (500 ,0);
-
-    if floor { 
-        max_depth += 2 ;
-        (0 .. 1000).for_each(|x| cave[max_depth][x] = 1)} ;
-        
-    loop {
-        if cave[0][500] == 1 || y >= max_depth {return sand} 
-        else if cave[y+1][x] == 0  {y += 1}
-        else if cave[y+1][x-1] == 1 && cave[y+1][x+1] == 1 {
-            cave[y][x] = 1 ;
-            sand += 1 ;
-            (x, y) = (500, 0)  ; 
-        } 
-        else if cave[y+1][x-1] == 0 { (x , y) = (x - 1, y + 1) }
-        else if cave[y+1][x+1] == 0 { (x , y) = (x + 1, y + 1) }
-    }
+    while cave[0][500] == 0 && y < max_depth + (2 * (floor as usize)) {
+        match (cave[y+1][x-1] , cave[y+1][x] , cave[y+1][x+1]) {
+            (_, 0, _ )  => y += 1 ,
+            (0, _, _)   => { (x , y) = (x - 1, y + 1) } ,
+            (_, _, 0)   => { (x , y) = (x + 1, y + 1) } ,
+            _           => { cave[y][x] = 1 ; (x, y) = (500, 0); sand += 1} ,
+            } 
+        }
+        sand
     }
