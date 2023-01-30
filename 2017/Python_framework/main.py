1b7ca3ded68bd5f6ea4bda74de38ec682e10fed3
Baptiste Cottier <42634014+baptistecottier@users.noreply.github.com>
2023-01-30 02:54:35 +0100

    2017 - Adding Python Framework

---
+++ 2017/Python_framework/main.py
@@ -0,0 +1,16 @@
+import sys
+import copy
+
+sys.path.append("../"+sys.argv[1][:2])
+module = __import__(sys.argv[1])
+
+
+
+if len(sys.argv) == 3 : path='../'+sys.argv[1][:2]+'/'+sys.argv[1][:2]+'_'+sys.argv[2]+'.txt'
+else : path='../'+sys.argv[1][:2]+'/'+sys.argv[1][:2]+'.txt'
+input_file=open(path, "r").read()
+
+
+input = module.generator(input_file) 
+print(module.part_1(copy.deepcopy(input)))
+print(module.part_2(input))
