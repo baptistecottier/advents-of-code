3ca5fc47e6b6f3a3ddce028507ef3559915f9cfb
Baptiste Cottier <42634014+baptistecottier@users.noreply.github.com>
2023-02-02 17:26:47 +0100

    2017 - 15 - Better arguments

--- 2017/15/15.py
+++ 2017/15/15.py
@@ -2,16 +2,14 @@ def generator(input):
     return [int(line.rsplit(' ',1)[-1]) for line in input.splitlines()]
 
 def part_1(input): 
-    return solver(input, False)
+    return solver(input, 40_000_000, 1, 1)
 
 def part_2(input): 
-    return solver(input, True)
+    return solver(input, 5_000_000, 4, 8)
 
-def solver(input, picky_generators): 
+def solver(input, rounds, mult_a, mult_b): 
     a, b = input
-    cnt = 0 
-    if picky_generators : (rounds, mult_a, mult_b) = (5_000_000, 4, 8)
-    else : (rounds, mult_a, mult_b) = (40_000_000, 1, 1)
+    matching_pairs = 0 
     for i in range(rounds):
         a = (a * 16807) % 2147483647
         while a % mult_a != 0 : a = (a * 16807) % 2147483647
@@ -19,6 +17,6 @@ def solver(input, picky_generators):
         b = (b * 48271) % 2147483647
         while b % mult_b != 0 : b = (b * 48271) % 2147483647
         
-        if a % (2 ** 16) == b % (2 ** 16): cnt += 1
-    return cnt
+        if a % (2 ** 16) == b % (2 ** 16): matching_pairs += 1
+    return matching_pairs
 
