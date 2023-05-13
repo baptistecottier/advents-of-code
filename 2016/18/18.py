882279afae0a82b125a260be983b71d67ee78616
Baptiste Cottier <42634014+baptistecottier@users.noreply.github.com>
2023-05-14 00:12:47 +0200

    2016 - 18 - Python: minor modificiations

--- 2016/18/18.py
+++ 2016/18/18.py
@@ -1,8 +1,8 @@
 def generator(input): return input
 
-def part_1(input): return solver(input, 38)
+def part_1(input): return solver(input, 40)
 
-def part_2(input): return solver(input, 40000)
+def part_2(input): return solver(input, 400_000)
 
 def tile_type(left, right):
     if (left == right) : return '.'
@@ -10,9 +10,9 @@ def tile_type(left, right):
 
 
 def solver(input, rep):
-    s='.'+input+'.' # We surrounds the input by dot to simulate safe tiles.
-    safe_tiles=s.count('.')-2 # We count the safe tiles without forgetting to substract the two sided safe tiles
-    for t in range(rep+1):
+    s='.'+input+'.'
+    safe_tiles=s.count('.')-2
+    for t in range(rep - 1):
         s='.'+''.join([tile_type(s[i]=='^',s[i+2]=='^') for i in range(len(s)-2)])+'.'
         safe_tiles+=s.count('.')-2
     return safe_tiles
