b1647347f8c4dcae425cab072b1c50e4fc9359b0
Baptiste Cottier <42634014+baptistecottier@users.noreply.github.com>
2023-05-14 00:01:02 +0200

    2021 - Setting up frameworks

--- 2020/Python/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-module = __import__(sys.argv[1])
-input_file=open('../inputs/'+sys.argv[1]+'.txt', "r").read()
-
-
-input = module.generator(input_file) 
-print(module.part_1(input))
-print(module.part_2(input))
