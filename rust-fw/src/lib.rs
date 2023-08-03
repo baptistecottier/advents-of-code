b4e2d73e91642b4cc117c60ebc24343de04d0353
Baptiste Cottier <42634014+baptistecottier@users.noreply.github.com>
2023-08-03 13:31:16 +0200

    New rust framework

---
+++ rust-fw/src/lib.rs
@@ -0,0 +1,97 @@
+use std::collections::HashSet;
+
+use itertools::Itertools;
+
+#[macro_export]
+macro_rules! main {
+    () => {
+        #[allow(unused_import)]
+        use {
+            aoc::screen_reader,
+            grid::Grid,
+            itertools::{chain, iproduct, repeat_n, FoldWhile, Itertools},
+            mod_exp::mod_exp,
+            serde_json::{json, Value},
+            std::cmp::Ordering,
+            std::collections::{HashMap, HashSet, VecDeque},
+            std::iter::zip,
+            std::ops::{Range, RangeInclusive},
+            std::str::FromStr,
+            std::env,
+            std::fs::read_to_string
+        };   
+
+        fn main() {
+            let year = &module_path!()[..4];
+            let day = &module_path!()[5..];
+            let puzzle_input = read_to_string(format!("../{}/{}/{}.input", year, day, day).trim_end()).expect("File does not exist") ;
+            let input = parser(puzzle_input.as_str());
+
+            println!(" <start>{}<end> ", part_1(input.clone()));
+            println!(" <start>{}<end> ", part_2(input));
+        }
+    };
+}
+
+pub fn rev_slice(input : &[u32]) -> Vec<u32> {
+    let mut a = input.to_vec() ;
+    a.reverse() ; 
+    a
+}
+
+pub fn screen_reader(pixels: HashSet<(usize, usize)>) -> String {
+    pixels
+    .iter()
+    .map(|(x, y)| 
+        (x - pixels.iter().min_by_key(|(x, _)| x).unwrap().0, 
+         y - pixels.iter().min_by_key(|(_, y)| y).unwrap().1))
+    .sorted()
+    .group_by(|(x, _)| x / 5)
+    .into_iter()
+    .map(|(k, v)| (k, v.map(|(x, y)| (x % 5, y)).collect::<Vec<(usize, usize)>>()))
+    .map(|(_, v)| 
+        match v.len() {
+            9 => 
+                if      v.contains(&(3, 0)) {'J'} 
+                else if v.contains(&(0, 5)) {'L'} 
+                else                        {'Y'}
+            10 =>                            'C',
+            11 =>
+                if      v.contains(&(0, 0)) {'F'}
+                else                        {'S'}
+            12 => 
+                if     !v.contains(&(0, 0)) {'O'}
+                else if v.contains(&(1, 2)) {'K'}
+                else if v.contains(&(3, 5)) {'Z'}
+                else if v.contains(&(3, 4)) {'U'}
+                else                        {'P'}
+            13 =>                            'G',
+            14 => 
+                if     !v.contains(&(0, 0)) {'A'}
+                else if v.contains(&(1, 3)) {'R'}
+                else if v.contains(&(2, 0)) {'E'}
+                else                        {'H'},
+            _  =>                            'B'})
+    .join("")
+
+}
+
+/*
+A: [(0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (1, 0), (1, 3), (2, 0), (2, 3), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5)]
+B: [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (1, 0), (1, 2), (1, 5), (2, 0), (2, 2), (2, 5), (3, 1), (3, 3), (3, 4)]
+C: [(0, 1), (0, 2), (0, 3), (0, 4), (1, 0), (1, 5), (2, 0), (2, 5), (3, 1), (3, 4)]
+E: [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (1, 0), (1, 2), (1, 5), (2, 0), (2, 2), (2, 5), (3, 0), (3, 5)]
+F: [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (1, 0), (1, 2), (2, 0), (2, 2), (3, 0)]
+G: [(0, 1), (0, 2), (0, 3), (0, 4), (1, 0), (1, 5), (2, 0), (2, 3), (2, 5), (3, 1), (3, 3), (3, 4), (3, 5)]
+H: [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (1, 2), (2, 2), (3, 0), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5)]
+J: [(0, 4), (1, 5), (2, 0), (2, 5), (3, 0), (3, 1), (3, 2), (3, 3), (3, 4)]
+K: [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (1, 2), (2, 1), (2, 3), (2, 4), (3, 0), (3, 5)]
+L: [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (1, 5), (2, 5), (3, 5)]
+O: [(0, 1), (0, 2), (0, 3), (0, 4), (1, 0), (1, 5), (2, 0), (2, 5), (3, 1), (3, 2), (3, 3), (3, 4)]
+P: [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (1, 0), (1, 3), (2, 0), (2, 3), (3, 1), (3, 2)]
+R: [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (1, 0), (1, 3), (2, 0), (2, 3), (2, 4), (3, 1), (3, 2), (3, 5)]
+S: [(0, 1), (0, 2), (0, 5), (1, 0), (1, 3), (1, 5), (2, 0), (2, 3), (2, 5), (3, 0), (3, 4)]
+U: [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (1, 5), (2, 5), (3, 0), (3, 1), (3, 2), (3, 3), (3, 4)]
+Y: [(2, 5), (0, 1), (2, 4), (1, 2), (0, 0), (3, 2), (4, 1), (2, 3), (4, 0)]
+Z: [(0, 0), (0, 4), (0, 5), (1, 0), (1, 3), (1, 5), (2, 0), (2, 2), (2, 5), (3, 0), (3, 1), (3, 5)]
+ */
