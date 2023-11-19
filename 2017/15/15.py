d2248032a3cf4884eb26a77f0831b577f58ae329
Baptiste Cottier <42634014+baptistecottier@users.noreply.github.com>
2023-11-19 12:18:17 +0100

    2017 - 15 - Python: Bug fix

--- 2017/15/15.py
+++ 2017/15/15.py
@@ -1,23 +1,19 @@
-from itertools import count
+def preprocessing(input):
+    items = input.split()
+    return int(items[4]), int(items[9])
 
-
-def preprocessing(input_):
-    return [int(line.rsplit(' ')[-1]) for line in input_.splitlines()]
-
-
-def solver(generators): 
-    a, b        = generators
-    old_count   = []
-    new_count_a = []
-    new_count_b = []
+def solver(genA, genB): 
+    first_logic_count = 0
+    second_logic_genA = []
+    second_logic_genB = []
     
-    while len(old_count) < 40_000_000 and all(len(count) < 5_000_000 for count in (new_count_a, new_count_b)):
-        a = (a * 16807) % 2147483647
-        b = (b * 48271) % 2147483647
-                         
-        old_count.append((a - b) % (2 ** 16) == 0)
-        if a % 4 == 0: new_count_a.append(a)
-        if b % 8 == 0: new_count_b.append(b)
+    for _ in range(40_000_000):
+        genA = (genA * 16807) % 2147483647
+        genB = (genB * 48271) % 2147483647
 
-    yield sum(old_count)
-    yield sum((a - b) % (2 ** 16) == 0 for a, b in zip(new_count_a, new_count_b))
+        if (genA - genB) % (2 ** 16) == 0: first_logic_count += 1
+        if genA % 4 == 0: second_logic_genA.append(genA)
+        if genB % 8 == 0: second_logic_genB.append(genB)
+
+    yield first_logic_count
+    yield sum((genA - genB) % (2 ** 16) == 0 for genA, genB in zip(second_logic_genA, second_logic_genB))
