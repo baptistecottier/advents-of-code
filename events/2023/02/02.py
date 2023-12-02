2f8c2e0096c5611501f2c417718e5cd4d305a6f7
baptistecottier <baptiste.cottier@gmail.com>
2023-12-02 12:55:10 +0100

    2023 - 02: Solutions

---
+++ events/2023/02/02.py
@@ -0,0 +1,22 @@
+import re
+
+def preprocessing(input):
+    game = list()
+    for line in input.splitlines():
+        draws = [(int(n), "bgr".index(c)) for n, c in re.findall(r'([0-9]+)\s+(b|g|r)', line)]
+        game.append(draws)
+    return game
+
+def solver(game):
+    power = 0
+    sumid = 0
+    for id, draws in enumerate(game, 1): 
+        if all(n + c < 15 for (n, c) in draws): sumid += id
+
+        mb = max(n for (n, c) in draws if c == 0)
+        mg = max(n for (n, c) in draws if c == 1)
+        mr = max(n for (n, c) in draws if c == 2)
+        power += mb * mg * mr
+
+    yield sumid
+    yield power
---
+++ events/2023/02/02.rs
@@ -0,0 +1,47 @@
+aoc::main!();
+
+fn preprocessing(input: &str) -> Vec<Vec<(usize, usize)>> {
+    input
+    .lines()
+    .map(|line| 
+        Regex::new(r"([0-9]+)\s+(b|g|r)")
+        .unwrap()
+        .captures_iter(line)
+        .map(|draw| draw.extract())
+        .map(|(_, [n, c])| 
+            match c {
+                "b" => (n.parse().unwrap(), 0),
+                "g" => (n.parse().unwrap(), 1),
+                "r" => (n.parse().unwrap(), 2),
+                _   => unreachable!()})
+        .collect_vec())
+    .collect()
+}
+
+fn part_1(game: Vec<Vec<(usize, usize)>>) -> usize {
+    game
+    .iter()
+    .enumerate()
+    .filter(|(_, draws)| 
+        draws
+        .iter()
+        .all(|(n, c)| n + c < 15))
+    .map(|(n, _)| n + 1)
+    .sum()
+}
+
+fn part_2(game: Vec<Vec<(usize, usize)>>) -> usize {
+    game
+    .iter()
+    .map(|draws| 
+        draws
+        .iter()
+        .fold((0, 0, 0), |(mb, mg, mr), (n, c)| 
+            match c {
+                0 => (mb.max(*n), mg        , mr        ),
+                1 => (mb        , mg.max(*n), mr        ),
+                2 => (mb        , mg        , mr.max(*n)),
+                _ => unreachable!()}))
+    .map(|(mb, mg, mr)| mb * mg * mr)
+    .sum()
+}
