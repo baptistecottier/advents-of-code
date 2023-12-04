66cfce46d11810ab97cc652226ac396c059cea90
baptistecottier <baptiste.cottier@gmail.com>
2023-12-05 00:30:57 +0100

    2023 - 04 - Rust: Solution

---
+++ events/2023/04/04.rs
@@ -0,0 +1,37 @@
+aoc::main!(); 
+
+fn preprocessing(input: &str) -> Vec<usize> {
+    input
+    .lines()
+    .map(|game| 
+        game
+        .split(&['|', ':'])
+        .skip(1)
+        .map(|numbers| 
+            numbers
+            .split_whitespace()
+            .map(|numbers| numbers.parse::<u8>().unwrap())
+            .collect::<HashSet<u8>>())
+        .collect_tuple().unwrap())
+    .map(|(draw, card)| draw.intersection(&card).count())
+    .collect_vec()
+}
+
+fn part_1(input: Vec<usize>) -> usize {
+    input
+    .into_iter()
+    .map(|n| (1 << n) >> 1)
+    .sum()
+}
+
+fn part_2(input: Vec<usize>) -> usize {
+    input
+    .clone()
+    .iter()
+    .enumerate()
+    .fold(vec![1; input.len()], |mut scratch, (game, score)| {
+        (game .. game + score).for_each(|gg| scratch[gg + 1] += scratch[game]);
+        scratch })
+    .iter()
+    .sum()
+}
