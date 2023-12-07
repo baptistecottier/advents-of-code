c775dc5ba8d5e101cf46ab8a19bd86618be00d7f
baptistecottier <baptiste.cottier@gmail.com>
2023-12-07 20:51:50 +0100

    2023 - 07 - Rust: Solution

---
+++ events/2023/07/07.rs
@@ -0,0 +1,36 @@
+aoc::main!(); 
+
+fn preprocessing(input: &str) -> Vec<(String, usize)> {
+    input
+    .lines()
+    .map(|hand| hand.split_once(' ').unwrap())
+    .map(|(cards, bet)| (cards.to_string(), bet.parse().unwrap()))
+    .collect()
+}
+
+fn part_1(bets: Vec<(String, usize)>) -> usize {
+    winnings(bets, '∅')
+    }
+    
+fn part_2(bets: Vec<(String, usize)>) -> usize {
+    winnings(bets, 'J')
+}
+
+fn winnings(bets: Vec<(String, usize)>, joker: char) -> usize {
+    bets
+    .iter()
+    .map(|(cards, bet)| (
+        cards.chars().filter(|c| *c == joker).count(), 
+        cards.chars().filter(|c| *c != joker).counts().into_values().sorted().rev().collect_vec(), 
+        cards.chars().map(|c| format!("{} {}", joker, "23456789TJQKA".replace(joker, "")).chars().position(|cc| cc == c).unwrap()).collect_vec(),
+        bet))
+    .map(|(jokers, hand_strength, cards_strength, bet)| (
+        5.min(jokers + hand_strength.iter().nth(0).unwrap_or(&0)), 
+        hand_strength.into_iter().skip(1).collect_vec(), 
+        cards_strength, 
+        bet))
+    .sorted()
+    .enumerate()
+    .map(|(n, (_, _, _, b))| (n + 1) * b)
+    .sum()
+}
