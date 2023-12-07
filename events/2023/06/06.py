0beed5c1e680419e01fbe0918d055d48c68c9d92
baptistecottier <baptiste.cottier@gmail.com>
2023-12-07 01:08:59 +0100

    2023 - 06 - Python: Optimisation

--- events/2023/06/06.py
+++ events/2023/06/06.py
@@ -1,23 +1,24 @@
-import re
-import numpy as np
+from re     import findall
+from numpy  import prod, roots
 
 def preprocessing(input):
     time, distance = input.splitlines()
 
-    time = re.findall(r'[0-9]+', time)
+    time = findall(r'[0-9]+', time)
     time.append(''.join(t for t in time))
     time = (int(t) for t in time)
 
-    distance = re.findall(r'[0-9]+', distance)
+    distance = findall(r'[0-9]+', distance)
     distance.append(''.join(d for d in distance))
     distance = (int(d) for d in distance)
 
-    return zip(time, distance)
+    ways = []
+    for time, distance in zip(time, distance):
+        l, h = sorted((int(n) for n in roots([1, - time, distance])))
+        ways.append(h - l)
+    return ways
 
-def solver(races):
-    total = 1
-    for time, distance in races:
-        l, h = sorted((int(n) for n in np.roots([1, - time, distance])))
-        total *= (h - l)
-    yield total // (h - l)
-    yield h - l
+
+def solver(ways):
+    yield (2, ways.pop())
+    yield (1, prod(ways))
