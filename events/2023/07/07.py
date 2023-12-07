a44d94050f81b31f3d9396dce2567277bd8ac05a
baptistecottier <baptiste.cottier@gmail.com>
2023-12-07 13:04:48 +0100

    2023 - 07 - Python: Optim

--- events/2023/07/07.py
+++ events/2023/07/07.py
@@ -6,30 +6,27 @@ def preprocessing(input):
 
 def solver(bets): 
     yield winnings(bets)    
-    yield winnings(bets, jkr = 'J')
+    yield winnings(bets, joker = 'J')
 
-def winnings(bets, jkr = '_'): 
+def winnings(bets, joker = '∅'): 
     hand_sort = []
-    card_strength = jkr + "23456789TJQKA".replace(jkr, '')
+    card_strength = {card: strength for strength, card in enumerate(joker + "23456789TJQKA".replace(joker, ''))}
     
     for cards, bet in bets: 
-        jokers = cards.count(jkr)
-        jcards = cards.replace(jkr, '')
+        jokers = cards.count(joker)
+        jcards = cards.replace(joker, '')
 
-        cnts = sorted(Counter(jcards).values(), reverse=True)
-        cnts += [0 for _ in range(5 - len(cnts))]        
+        hand_strength = sorted(Counter(jcards).values(), reverse=True)
+        hand_strength += [0 for _ in range(5 - len(hand_strength))]        
 
         i = 0
         while jokers > 0:
-            if cnts[i] < 5:
-                cnts[i] += 1
+            if hand_strength[i] < 5:
+                hand_strength[i] += 1
                 jokers -= 1
             else : i += 1
 
-        hand_strength = list(Counter(cnts).items())
-        cards_strength = [card_strength.index(card) for card in cards]
-
+        cards_strength = [card_strength[card] for card in cards]
         hand_sort.append((hand_strength, cards_strength, bet))
  
-    hand_sort = sorted(hand_sort)
-    return sum(n * b for  n, (_, _, b) in enumerate(hand_sort, 1))
+    return sum(n * b for  n, (_, _, b) in enumerate(sorted(hand_sort), 1))
