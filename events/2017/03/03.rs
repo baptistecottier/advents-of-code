8a1bacef2fce49199405acde687c364f1e2499d5
baptistecottier <baptiste.cottier@gmail.com>
2024-01-23 23:26:15 +0100

    2017 - 03 - Rust : Solving part 2

--- events/2017/03/03.rs
+++ events/2017/03/03.rs
@@ -1,14 +1,27 @@
 aoc::main!();
 
-fn preprocessing(input: &str) -> u32 {
+fn preprocessing(input: &str) -> i32 {
     input.parse().unwrap()
 }
 
-fn part_1(square_position: u32) -> u32 {
-    let circle_index = (1.0 + ((square_position - 1) as f32).sqrt() / 2.0) as u32;
+fn part_1(square_position: i32) -> i32 {
+    let circle_index = (1.0 + ((square_position - 1) as f32).sqrt() / 2.0) as i32;
     circle_index + (square_position - 1) % circle_index 
 }
 
-fn part_2(square_position: u32) -> u32 {
-    square_position
+fn part_2(square_position: i32) -> i32 {
+    let (mut x, mut y) = (0, 0);
+    let mut memory : HashMap<(i32, i32), i32>= HashMap::new();
+    let (mut dx, mut dy) = (0, -1);
+    let mut value = 1 ; 
+    let turn = HashMap::from([((1,0), (0, 1)), ((0, 1), (-1, 0)), ((-1,0), (0, -1)), ((0, -1), (1, 0))]) ; 
+    while value < square_position {
+        let (tx, ty) = turn[&(dx, dy)];
+        memory.insert((x, y), value) ;
+        if !memory.contains_key(&(x + tx, y + ty)){
+            (dx, dy) = (tx, ty)};
+        (x, y) = (x + dx, y + dy) ;
+        value = iproduct!(x - 1..x + 2, y-1..y + 2).map(|(a, b)| memory.get(&(a, b)).unwrap_or(&0)).sum::<i32>();
+    };
+    value
 }
