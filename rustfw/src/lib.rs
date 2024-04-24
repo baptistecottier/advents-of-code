43c9292f74fdd50e82df9778a73650ff4a2593f3
baptistecottier <baptiste.cottier@gmail.com>
2024-04-24 23:03:41 +0200

    cleaning lib.rs

--- rustfw/src/lib.rs
+++ rustfw/src/lib.rs
@@ -1,86 +1,121 @@
-use std::collections::HashSet;
 use itertools::Itertools;
+use std::collections::HashSet;
 
 #[macro_export]
 macro_rules! main {
     () => {
         #[allow(unused_import)]
         use {
-            roots::Roots,
-            roots::find_roots_quadratic,
+            aho_corasick::{AhoCorasick, PatternID},
             aoc::screen_reader,
             grid::Grid,
-            itertools::{chain, iproduct, repeat_n, multizip, FoldWhile, Itertools},
+            itertools::{chain, iproduct, multizip, repeat_n, FoldWhile, Itertools},
+            md5::compute,
             mod_exp::mod_exp,
             num::pow,
             regex::Regex,
-            aho_corasick::{AhoCorasick, PatternID},
+            roots::find_roots_quadratic,
+            roots::Roots,
             serde_json::{json, Value},
             std::cmp::Ordering,
             std::collections::{HashMap, HashSet, VecDeque},
+            std::env,
+            std::fs::read_to_string,
             std::iter::{successors, zip},
             std::ops::{Range, RangeInclusive},
             std::str::FromStr,
-            std::env,
-            std::fs::read_to_string
-        };   
+        };
 
-        fn main() {            
+        fn main() {
             let year = &module_path!()[..4];
             let day = &module_path!()[5..];
-            let puzzle_input = read_to_string(format!("../events/{}/{}/{}.input", year, day, day).trim_end()).expect("File does not exist") ;
-            
-            let input = preprocessing(puzzle_input.as_str()); 
+            let puzzle_input =
+                read_to_string(format!("../events/{}/{}/{}.input", year, day, day).trim_end())
+                    .expect("File does not exist");
 
-            println!(" <start>{}<end> ", part_1(input.clone()));
-            println!(" <start>{}<end> ", part_2(input));
+            let input = preprocessing(puzzle_input.as_str());
+
+            println!("{:?}", (part_1(input.clone()), part_2(input)));
         }
     };
 }
 
 pub fn rev_slice(input: &[u32]) -> Vec<u32> {
-    let mut a = input.to_vec() ;
-    a.reverse() ; 
+    let mut a = input.to_vec();
+    a.reverse();
     a
 }
 
 pub fn screen_reader(pixels: HashSet<(usize, usize)>) -> String {
     pixels
-    .iter()
-    .map(|(x, y)| 
-        (x - pixels.iter().min_by_key(|(x, _)| x).unwrap().0, 
-         y - pixels.iter().min_by_key(|(_, y)| y).unwrap().1))
-    .sorted()
-    .group_by(|(x, _)| x / 5)
-    .into_iter()
-    .map(|(k, v)| (k, v.map(|(x, y)| (x % 5, y)).collect::<Vec<(usize, usize)>>()))
-    .map(|(_, v)| 
-        match v.len() {
-            9 => 
-                if      v.contains(&(3, 0)) {'J'} 
-                else if v.contains(&(0, 5)) {'L'} 
-                else                        {'Y'}
-            10 =>
-                if      v.contains(&(0, 0)) {'I'}
-                else                        {'C'}
-            11 =>
-                if      v.contains(&(0, 0)) {'F'}
-                else                        {'S'}
-            12 => 
-                if     !v.contains(&(0, 0)) {'O'}
-                else if v.contains(&(1, 2)) {'K'}
-                else if v.contains(&(3, 5)) {'Z'}
-                else if v.contains(&(3, 4)) {'U'}
-                else                        {'P'}
-            13 =>                            'G',
-            14 => 
-                if     !v.contains(&(0, 0)) {'A'}
-                else if v.contains(&(1, 3)) {'R'}
-                else if v.contains(&(2, 0)) {'E'}
-                else                        {'H'},
-            _  =>                            'B'})
-    .join("")
-
+        .iter()
+        .map(|(x, y)| {
+            (
+                x - pixels.iter().min_by_key(|(x, _)| x).unwrap().0,
+                y - pixels.iter().min_by_key(|(_, y)| y).unwrap().1,
+            )
+        })
+        .sorted()
+        .group_by(|(x, _)| x / 5)
+        .into_iter()
+        .map(|(k, v)| {
+            (
+                k,
+                v.map(|(x, y)| (x % 5, y)).collect::<Vec<(usize, usize)>>(),
+            )
+        })
+        .map(|(_, v)| match v.len() {
+            9 => {
+                if v.contains(&(3, 0)) {
+                    'J'
+                } else if v.contains(&(0, 5)) {
+                    'L'
+                } else {
+                    'Y'
+                }
+            }
+            10 => {
+                if v.contains(&(0, 0)) {
+                    'I'
+                } else {
+                    'C'
+                }
+            }
+            11 => {
+                if v.contains(&(0, 0)) {
+                    'F'
+                } else {
+                    'S'
+                }
+            }
+            12 => {
+                if !v.contains(&(0, 0)) {
+                    'O'
+                } else if v.contains(&(1, 2)) {
+                    'K'
+                } else if v.contains(&(3, 5)) {
+                    'Z'
+                } else if v.contains(&(3, 4)) {
+                    'U'
+                } else {
+                    'P'
+                }
+            }
+            13 => 'G',
+            14 => {
+                if !v.contains(&(0, 0)) {
+                    'A'
+                } else if v.contains(&(1, 3)) {
+                    'R'
+                } else if v.contains(&(2, 0)) {
+                    'E'
+                } else {
+                    'H'
+                }
+            }
+            _ => 'B',
+        })
+        .join("")
 }
 
 /*
