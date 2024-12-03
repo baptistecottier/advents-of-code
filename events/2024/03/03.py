5f8512e1fa5dea76e6ec249d1850d274a4fbd2dc
Baptiste Cottier <baptiste.cottier@icloud.com>
2024-12-03 08:54:04 +0100

    2024 - 03 : Python solution, input and puzzle description

---
+++ events/2024/03/03.py
@@ -0,0 +1,31 @@
+import re
+
+def preprocessing(puzzle_input):
+    """_summary_
+    Take the initial input and remove carriage return as they stop regex expressions
+    """
+    return puzzle_input.replace("\n", "")
+
+def solver(program):
+    """
+    For the first part, scan the program for uncorrupted mul operations. 
+    Then remove all don't()...do() enclosure and remove the last disabled instructions following a don't()
+    """
+    yield scan_mul(program)
+    program = re.sub(r"don't\(\)(.*?)do\(\)", "", program) # Removing all don't()...do() enclosures
+    program = program.split("don't()", 1)[0] # Removing enabled instruction at the end
+    yield scan_mul(program)
+    
+def scan_mul(program):
+    """
+    Use regex expression to extract uncorrupted instructions.
+    \d{1,3} allows to extract only numbers with one to three digits.
+    Then add the product of matched pairs and return it
+    """
+    score = 0
+    uncorrupted = re.findall(r"mul\((\d{1,3}?),(\d{1,3}?)\)", program)
+    for a, b in uncorrupted: 
+        score += int(a) * int(b)
+    return score
+
+    
