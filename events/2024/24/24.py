ca036a59463c24b0a9ac5337f7cbc22f701cd3bd
baptistecottier <baptiste.cottier@icloud.com>
2024-12-25 23:14:51 +0100

    2024 - 24 :  Explanations for part 2

--- events/2024/24/24.py
+++ events/2024/24/24.py
@@ -2,9 +2,11 @@ def preprocessing(puzzle_input):
     inits, ops = puzzle_input.split('\n\n')
     wires = {}
     connections = []
+    
     for init in inits.splitlines():
         w, v = init.split(': ')
         wires[w] = int(v)   
+        
     for op in ops.splitlines():
         (a, o, b, _, c) = op.split(' ')
         wires[c] = None
@@ -15,103 +17,155 @@ def preprocessing(puzzle_input):
     return wires, connections
 
 
-def solver(wires, connections):
-    ix = ""
-    iy = ""
-
-    for k, v in sorted(wires.items()):
-        if k.startswith('x'):
-            ix += str(v)
-        if k.startswith('y'):
-            iy += str(v)
-    ix = int(ix[::-1], 2)
-    iy = int(iy[::-1], 2)
-    z = compute(wires, connections)
-    yield z 
-    yield "cvp,mkk,qbw,wcb,wjb,z10,z14,z34"
+def solver(wires: dict, connections):
+    yield (compute(wires, connections))
+    yield manual_debug(wires, connections, explanations = False)
+    
     
 def compute(wires, connections):
     while None in wires.values():
         for a, b, c, f in connections:
             if all(wires[w] != None for w in (a, b)):
                 wires[c] = f(wires, a, b)
-            # print(c)
     s = ""
     for k, v in sorted(wires.items()):
         if k.startswith('z'):
-            s += str(v)
-    z = int(s[::-1], 2)
+            s = str(v) + s
+    z = int(s, 2)
     return z
 
-###     All what appears below are traces of what has been used to solve part 2    ###
-    # print(str(bin(ix)))
-    # print(str(bin(iy)))
-    # print(str(bin(iy + ix)))
-    # print(str(bin(z)))
-    # z11, 
-# mkk, z10, z14, qbw, 
-# 
-# cvp, wjb, z34, mpd
-# cvp,mkk,mpd,qbw,wcb,z10,z14,z34
-# 0b100001100010111111111 0101011110010010001110000
-# 0b100001100100000000000 0101011110010010001110000
+def manual_debug(wires, connections, explanations):
+    """ Explanations for the manual solve of part 2.
+    
+    First print the binary value of x + y and z and look for the first different bit:"""
+    if explanations:
+        display_x_plus_y_and_z(wires, connections)
+        """
+        0b1000011000101111111110101011110010010001110000
+        0b1000011001000000000000101011101110000001110000
+                                            ↑
+                            First different bit at index 10
+        Let' see how z10 is computed:
+                            mvs AND jvj -> z10
+        and compare it to computations of z09, z08, etc.
+                            nvv XOR (y09 XOR x09) -> z09
+                            (x08 XOR y08) XOR fhf -> z08
+        We can guess z10 is supposed to be computed as 
+                            (x10 XOR y10) XOR ??? -> z10
+        We have y10 XOR x10 -> jvj and jvj XOR mvs -> mkk, so:
+                            (y10 XOR x10) XOR mvs -> mkk
+        Swapping z10 with mkk seems relevant:"""
+        connections = swap_connections(connections, 'z10', 'mkk')
+        
+        print("\nAfter first swap")
+        display_x_plus_y_and_z(wires, connections)
+        
+        """
+        0b1000011000101111111110101011110010010001110000
+        0b1000011001000000000000101011101110010001110000
+                                         ↑
+                            First different bit at index 14
+        z14 is computed as vjh OR fhq -> z14 and applying the same logic as above, 
+        we have:
+                            ndm XOR (x14 XOR y14) -> qbw
+        Hence we swap qbw with z14."""
+        connections = swap_connections(connections, 'z14', 'qbw')
+        
+        print("\nAfter second swap")
+        display_x_plus_y_and_z(wires, connections)
+        """
+        0b1000011000101111111110101011110010010001110000
+        0b1000011001000000000000101011110010010001110000
+                              ↑
+        First different bit at index 25. A lot of following values are also 
+        different hence we can deduce the carry is wrong here.
+        (y24 XOR x24) XOR ((bfq AND bjc) OR rgp) -> z24
+        (x25 AND y25) XOR ((x24 AND y24) OR mcr) -> z25
+        Left operand should be (x25 XOR y25). We have 
+                x25 XOR y25 -> wjb and x25 AND y25 -> cvp
+        so we swap wjb with cvp."""
+        connections = swap_connections(connections, 'wjb', 'cvp')
+        
+        print("\nAfter third swap")
+        display_x_plus_y_and_z(wires, connections)
+        """
+        0b111000100101111101010100000111011010111001100
+        0b111000100011111101010100000111011010111001100
+                    ↑
+        First different bit at index 34
+        We have x + y = z but that does not mean the problem is solved as we know 
+        there are four swaps. To find the last one, we have to modify the values of x
+        or y.
+        
+        """
+        print("\nUsing random x and y")
+        display_x_plus_y_and_z(wires, connections, randxy = True)
+        """
+        0b111000100101111101010100000111011010111001100
+        0b111000100011111101010100000111011010111001100
+        z34 is computed as y34 AND x34 -> z34 and applying the same logic as in the 
+        two first cases, we have:
+                            (y34 XOR x34) XOR mdh -> wcb.
+        Hence, wcb should be z34 and we swap those values."""
+        connections = swap_connections(connections, 'z34', 'wcb')
+        
+        print("\nAfter fourth swap")
+        display_x_plus_y_and_z(wires, connections)
+        """
+        0b1100000111110010101001111011100100100101011110
+        0b1100000111110010101001111011100100100101011110
+        
+        Let's check using randoms x and y:
+        """
+        display_x_plus_y_and_z(wires, connections, randxy = True)
+        """
+        0b1100110100100000100000101110000111110100000100
+        0b1100110100100000100000101110000111110100000100
+        """
+        display_x_plus_y_and_z(wires, connections, randxy = True)
+        """
+        0b1011001001011100110001101100111010011011010111
+        0b1011001001011100110001101100111010011011010111
+        """
+        display_x_plus_y_and_z(wires, connections, randxy = True)
+        """
+        0b11101100111001111111101101110011111001111010
+        0b11101100111001111111101101110011111001111010
+        
+        Everything seems fixed, the four swaps are:
+        - z10 with mkk
+        - z14 with qbw
+        - wjb with cvp
+        - z34 with wcb
+        
+        Sorting them alphabetically results in cvp,mkk,qbw,wcb,wjb,z10,z14,z34
+        """
+    return "cvp,mkk,qbw,wcb,wjb,z10,z14,z34"
 
-# 0b100001001110 1000100010011001101001100100101001
-# 0b100001001101 1000100010011001101001100100101001
-
-
-# kvd good
-
-"""
-(x25 AND y25) XOR (jfm OR mcr) -> z25
-
-cvp XOR fqv -> z25
-
-x25 AND y25 -> cvp
-jfm OR mcr -> fqv
-"""
-
-
-# 0b111101100010111011101100110010110011011010101
-# 0b111101100100111011101100110010110011011010101
-
-
-"z10 mkk"
-
-"""
-vjh OR fhq -> z14
-x14 XOR y14 -> tsp
-cvp
-wjb
-nvv XOR kvd -> z09
-y09 XOR x09 -> kvd
-dkk OR tqj -> nvv
-
-mvs AND jvj -> z10
-
-((((x07 AND y07) OR ((07 XOR x07) AND tvn)) AND (x08 XOR y08)) OR (y08 AND x08)) XOR (y09 XOR x09) -> z09
-
-y09 XOR x09 -> kvd
-
-dkk OR tqj -> nvv
-fhf AND btf -> dkk
-y08 AND x08 -> tqj
-
-y10 XOR x10 -> jvj
-
-
-((x09 AND y09) OR (nvv AND (y09 XOR x09))) AND (y10 XOR x10) -> z10
-
-y10 XOR x10 -> jvj
-
-mqk OR vbs -> mvs
-nvv AND kvd -> vbs
-dkk OR tqj -> nvv
-
-y09 XOR x09 -> kvd
-x09 AND y09 -> mqk
-z34, 
-0b11110110001 0111011101100110010110011011010101
-0b11110110010 0111011101100110010110011011010101
-
-"""
+def swap_connections(connections, swap1, swap2):
+    new_connections = []
+    for (a, b, c, f) in connections:
+        if c == swap1:
+            c = swap2
+        elif c == swap2:
+            c = swap1
+        new_connections.append((a, b, c, f))
+    return new_connections
+        
+from random import random
+def display_x_plus_y_and_z(wires, connections, randxy = False):
+    x, y = "", ""
+    for k, v in sorted(wires.items(), reverse=True):
+        if k.startswith('x'): 
+            if randxy:
+                v = round(random())
+                wires[k] = v
+            x += str(v)
+        if k.startswith('y'):
+            if randxy:
+                v = round(random())
+                wires[k] = v
+            y += str(v)
+    x, y = int(x, 2), int(y, 2)
+    print(f"x + y = {bin(y + x)}")
+    print(f"    z = {bin(compute(wires, connections))}")
