e78dde087d5dba74ba7612a04a5b9e2c08fbd57d
Baptiste Cottier <baptiste.cottier@icloud.com>
2025-07-28 00:14:45 +0200

    Moving files

--- events/2019/09/09.py
+++
@@ -1,8 +0,0 @@
-from pythonfw.ship_computer import Program
-
-def preprocessing(puzzle_input): 
-    return list(map(int, puzzle_input.split(',')))
-
-def solver(intcodes): 
-    yield Program(intcodes).run(1)
-    yield Program(intcodes).run(2)
