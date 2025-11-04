9f3a608b8bf65d5624b84645999ad4138cd72be6
Baptiste Cottier <baptiste.cottier@inria.fr>
2025-11-04 13:07:41 +0100

    [2025] - Adding files

---
+++ events/year_2025/day_01/day_01.rs
@@ -0,0 +1,15 @@
+aoc::main!();
+
+fn preprocessing(input: &str) -> Vec<&str> {
+    input.lines().collect()
+}
+
+fn part_1(data: Vec<&str>) -> usize {
+    // TODO: Implement part 1
+    0
+}
+
+fn part_2(data: Vec<&str>) -> usize {
+    // TODO: Implement part 2
+    0
+}
---
+++ events/year_2025/day_11/day_11.py
@@ -0,0 +1,25 @@
+"""
+Advent of Code - Year 2025 - Day 11
+https://adventofcode.com/2025/day/11
+"""
+
+
+def preprocessing(puzzle_input: str) -> list[str]:
+    """
+    Processes the puzzle input into a suitable data structure.
+    """
+    return puzzle_input.strip().split('\n')
+
+
+def solver(data: list[str]) -> tuple[int, int]:
+    """
+    Solves both parts of the puzzle.
+    Returns a tuple (part1_result, part2_result).
+    """
+    # TODO: Implement part 1
+    part1 = 0
+    
+    # TODO: Implement part 2  
+    part2 = 0
+    
+    return part1, part2
