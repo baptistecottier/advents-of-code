fa88484b26fd435f2d2e2357dd7bf0b679d1214b
Baptiste Cottier <baptiste.cottier@inria.fr>
2025-12-02 16:16:14 +0100

    [2025][day 1] fix

--- events/year_2025/day_01/day_01.py
+++ events/year_2025/day_01/day_01.py
@@ -5,10 +5,6 @@ https://adventofcode.com/2025/day/1
 
 
 def preprocessing(puzzle_input: str) -> list[int]:
-    """
-    Converts puzzle input into a list of signed integers based on rotation direction.
-    """
-
     instructions: list[int] = []
     for line in puzzle_input.splitlines():
         way, clicks = line[0], int(line[1:])
@@ -22,26 +18,24 @@ def preprocessing(puzzle_input: str) -> list[int]:
 
 def solver(instructions: list[int]) -> tuple[int, int]:
     """
-    Solves the puzzle by processing a list of integer instructions and 
-    returns the counts of times the dial is left pointing at 0 after any rotation in the sequence 
-    and then counts of time the dial points at 0.
+    Solves both parts of the puzzle.
+    Returns a tuple (part1_result, part2_result).
     """
-
     rotation = 50
+    zero_cnt = 0
     pass_cnt = 0
-    stop_cnt = 0
 
     for instruction in instructions:
         if rotation == 0 and instruction < 0:
             rotation = 100
 
         rotation += instruction
-        pass_cnt += abs(abs(rotation) // 100)
+        pass_cnt += abs(rotation // 100)
         rotation %= 100
 
         if rotation == 0:
             if instruction < 0:
                 pass_cnt += 1
-            stop_cnt += 1
+            zero_cnt += 1
 
-    return stop_cnt, pass_cnt
+    return zero_cnt, pass_cnt
