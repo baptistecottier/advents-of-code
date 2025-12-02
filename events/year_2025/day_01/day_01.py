0b14c3aee2baffa45e08f090bbbcc8b7a1381193
Baptiste Cottier <baptiste.cottier@inria.fr>
2025-12-02 15:54:06 +0100

    [2025][Python] days 1 and 2

--- events/year_2025/day_01/day_01.py
+++ events/year_2025/day_01/day_01.py
@@ -4,22 +4,47 @@ https://adventofcode.com/2025/day/1
 """
 
 
-def preprocessing(puzzle_input: str) -> list[str]:
-    """
-    Processes the puzzle input into a suitable data structure.
-    """
-    return puzzle_input.strip().split('\n')
+from networkx import bellman_ford_path
 
 
-def solver(data: list[str]) -> tuple[int, int]:
+def preprocessing(puzzle_input: str) -> list[int]:
     """
-    Solves both parts of the puzzle.
-    Returns a tuple (part1_result, part2_result).
+    Converts puzzle input into a list of signed integers based on rotation direction.
     """
-    # TODO: Implement part 1
-    part1 = 0
-    
-    # TODO: Implement part 2  
-    part2 = 0
-    
-    return part1, part2
+
+    instructions: list[int] = []
+    for line in puzzle_input.splitlines():
+        way, clicks = line[0], int(line[1:])
+        if way == 'R':
+            instructions.append(clicks)
+        else:
+            instructions.append(-clicks)
+
+    return instructions
+
+
+def solver(instructions: list[int]) -> tuple[int, int]:
+    """
+    Solves the puzzle by processing a list of integer instructions and 
+    returns the counts of times the dial is left pointing at 0 after any rotation in the sequence 
+    and then counts of time the dial points at 0.
+    """
+
+    rotation = 50
+    pass_cnt = 0
+    stop_cnt = 0
+
+    for instruction in instructions:
+        if rotation == 0 and instruction < 0:
+            rotation = 100
+
+        rotation += instruction
+        pass_cnt += abs(abs(rotation) // 100)
+        rotation %= 100
+
+        if rotation == 0:
+            if instruction < 0:
+                pass_cnt += 1
+            stop_cnt += 1
+
+    return stop_cnt, pass_cnt
--- events/year_2025/day_02/day_02.py
+++ events/year_2025/day_02/day_02.py
@@ -3,23 +3,47 @@ Advent of Code - Year 2025 - Day 2
 https://adventofcode.com/2025/day/2
 """
 
-
-def preprocessing(puzzle_input: str) -> list[str]:
-    """
-    Processes the puzzle input into a suitable data structure.
-    """
-    return puzzle_input.strip().split('\n')
+from collections import defaultdict
 
 
-def solver(data: list[str]) -> tuple[int, int]:
+def preprocessing(puzzle_input: str) -> list[tuple[int, int]]:
     """
-    Solves both parts of the puzzle.
-    Returns a tuple (part1_result, part2_result).
+    Parses a comma-separated string of integer ranges into a list of tuples.
     """
-    # TODO: Implement part 1
-    part1 = 0
-    
-    # TODO: Implement part 2  
-    part2 = 0
-    
-    return part1, part2
+    ranges = puzzle_input.split(',')
+    lst_ranges = []
+    for rng in ranges:
+        lst_ranges.append(tuple(map(int, rng.split('-'))))
+    return lst_ranges
+
+
+def solver(ranges: list[tuple[int, int]]) -> tuple[int, int]:
+    """
+    Calculates the sum of product IDs with repeated digit patterns in given ranges and returns the
+    sum for pattern repeted twice and the total sum for all patterns appearing.
+    """
+    invalid_register = defaultdict(int)
+
+    for start, end in ranges:
+        for product_id in range(start, end + 1):
+            str_id = str(product_id)
+            half_length = len(str_id) // 2
+
+            # We need to start by 2 to ensure cases as '9999'
+            # won't be counted for length 1
+            if str_id[:half_length] * 2 == str_id:
+                invalid_register[2] += product_id
+                continue
+
+            if str_id[0] * len(str_id) == str_id:
+                invalid_register[1] += product_id
+                continue
+
+            for i in range(2, half_length + 1):
+                if len(str_id) % i != 0:
+                    continue
+                if str_id[:i] * (len(str_id) // i) == str_id:
+                    invalid_register[(len(str_id) // i)] += product_id
+                    break
+
+    return invalid_register[2], sum(invalid_register.values())
