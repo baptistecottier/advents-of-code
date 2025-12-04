8e6eb7124a3457b73cf849c55d7a6e6b4be5b0db
Baptiste Cottier <baptiste.cottier@inria.fr>
2025-12-04 10:34:34 +0100

    [2025][Python][04] add solution

--- events/year_2025/day_04/day_04.py
+++ events/year_2025/day_04/day_04.py
@@ -4,22 +4,32 @@ https://adventofcode.com/2025/day/4
 """
 
 
-def preprocessing(puzzle_input: str) -> list[str]:
+def preprocessing(puzzle_input: str) -> set[tuple[int, int]]:
     """
-    Processes the puzzle input into a suitable data structure.
+    Parse puzzle input and return the set of coordinates where '@' characters are found.
     """
-    return puzzle_input.strip().split('\n')
+    roll = set()
+    for y, line in enumerate(puzzle_input.splitlines()):
+        for x, c in enumerate(line):
+            if c == '@':
+                roll.add((x, y))
+
+    return roll
 
 
-def solver(data: list[str]) -> tuple[int, int]:
+def solver(rolls: set[tuple[int, int]]) -> tuple[int, int]:
     """
-    Solves both parts of the puzzle.
-    Returns a tuple (part1_result, part2_result).
+    Iteratively removes positions with fewer than 4 neighbors until no more can be removed.
+    Returns the count removed in the first iteration and the total count removed.
     """
-    # TODO: Implement part 1
-    part1 = 0
-    
-    # TODO: Implement part 2  
-    part2 = 0
-    
-    return part1, part2
+    removed = [1, -1] # Set as it to pass the while predicate and not modifying the final sum
+    neighbours = ((-1, -1), (0, -1), (1, -1),
+                  (-1, 0),           (1, 0),
+                  (-1, 1),  (0, 1),  (1, 1))
+    while removed[-1] != 0:
+        to_remove = {(rx, ry) for rx, ry in rolls
+                     if sum((rx + dx, ry + dy) in rolls for (dx, dy) in neighbours) < 4}
+        removed.append(len(to_remove))
+        rolls = rolls.difference(to_remove)
+
+    return removed[2], sum(removed)
