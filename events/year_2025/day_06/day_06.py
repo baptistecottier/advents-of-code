355e92bd9e2b0c79f24402ea4037af5b7e08e881
Baptiste Cottier <baptiste.cottier@inria.fr>
2025-12-08 22:26:47 +0100

    [2025][Python][06] add solution

--- events/year_2025/day_06/day_06.py
+++ events/year_2025/day_06/day_06.py
@@ -3,23 +3,39 @@ Advent of Code - Year 2025 - Day 6
 https://adventofcode.com/2025/day/6
 """
 
-
-def preprocessing(puzzle_input: str) -> list[str]:
-    """
-    Processes the puzzle input into a suitable data structure.
-    """
-    return puzzle_input.strip().split('\n')
+from math import prod
+from typing import Callable
 
 
-def solver(data: list[str]) -> tuple[int, int]:
+def preprocessing(puzzle_input: str) -> list[tuple[list[list[str]], Callable]]:
     """
-    Solves both parts of the puzzle.
-    Returns a tuple (part1_result, part2_result).
+    Returns a list of tuples where each tuple contains a matrix of digits and its operation
+    function.
     """
-    # TODO: Implement part 1
-    part1 = 0
-    
-    # TODO: Implement part 2  
-    part2 = 0
-    
-    return part1, part2
+    lines = puzzle_input.splitlines()
+    op = lines[-1].split()
+    problems = []
+    problem = [[] for _ in range(len(lines) - 1)]
+    for i in range(len(lines[0])):
+        if all(line[i] == ' ' for line in lines):
+            problems.append((problem, sum if op.pop(0) == '+' else prod))
+            problem = [[] for _ in range(len(lines) - 1)]
+        else:
+            for j in range(len(lines) - 1):
+                problem[j] += lines[j][i]
+    problems.append((problem, sum if op.pop(0) == '+' else prod))
+    return problems
+
+
+def solver(problems: list[tuple[list[list[str]], Callable]]) -> tuple[int, int]:
+    """
+    Calculates horizontal and vertical sums by applying operations to problem data.
+    Returns a tuple containing the total horizontal and vertical results.
+    """
+    horizontal = 0
+    vertical = 0
+    for problem, op in problems:
+        horizontal += op(int("".join(line)) for line in problem)
+        vertical += op(int("".join(line)) for line in list(map(list, zip(*problem))))
+
+    return horizontal, vertical
