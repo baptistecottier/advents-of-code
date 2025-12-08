27108f16be67e13c0b1767725b6aa113ce82b423
Baptiste Cottier <baptiste.cottier@inria.fr>
2025-12-08 22:27:07 +0100

    [2025][Python][08] add solution

--- events/year_2025/day_08/day_08.py
+++ events/year_2025/day_08/day_08.py
@@ -4,22 +4,67 @@ https://adventofcode.com/2025/day/8
 """
 
 
-def preprocessing(puzzle_input: str) -> list[str]:
+def euclidean_distance(pt_a, pt_b):
     """
-    Processes the puzzle input into a suitable data structure.
+    Calculate the Euclidean distance between two points in n-dimensional space.
     """
-    return puzzle_input.strip().split('\n')
+    return sum((a - b) ** 2 for a, b in zip(pt_a, pt_b)) ** 0.5
 
 
-def solver(data: list[str]) -> tuple[int, int]:
+def preprocessing(puzzle_input: str
+                  ) -> tuple[dict[float, tuple[tuple[int, int], tuple[int, int]]], int]:
+    """
+    Parse input to create distance-coordinate mapping and return point pairs with their euclidean
+    distances.
+    """
+    pts = set()
+    coordinates = {}
+    for line in puzzle_input.splitlines():
+        pt_a = tuple(map(int, line.split(',')))
+        for pt_b in pts:
+            coordinates[euclidean_distance(pt_a, pt_b)] = (pt_a, pt_b)
+        pts.add(pt_a)
+    return (coordinates, len(puzzle_input.splitlines()))
+
+
+def solver(coordinates: dict[float, tuple[tuple[int, int], tuple[int, int]]], l_dist: int
+           )-> tuple[int, int]:
     """
     Solves both parts of the puzzle.
     Returns a tuple (part1_result, part2_result).
     """
-    # TODO: Implement part 1
-    part1 = 0
-    
-    # TODO: Implement part 2  
-    part2 = 0
-    
-    return part1, part2
+    distances = sorted(coordinates)
+    circuits: list[set] = []
+    checkprod = l_dist
+
+    for n in range(len(coordinates)):
+
+        pt = coordinates[distances[n]]
+        print(pt)
+        circuits = update_circuits(circuits, pt)
+
+        if l_dist in (len(c) for c in circuits):
+            return checkprod, pt[0][0] * pt[1][0]
+
+        if n == 999:
+            l = sorted([len(c) for c in circuits])
+            checkprod = l[-1] * l[-2] * l[-3]
+
+    raise ValueError("Impossible to connect all pairs")
+
+
+def update_circuits(circuits, pt):
+    """
+    Updates circuit connections by merging circuits that share nodes with the given point pair.
+    """
+    for circuit in circuits[:]:
+        for b in [1, 0]:
+            if pt[b] in circuit:
+                for circuit2 in circuits[:]:
+                    if pt[1 - b] in circuit2 and circuit2 != circuit:
+                        circuit.update(circuit2)
+                        circuits.remove(circuit2)
+                circuit.add(pt[1 - b])
+                return circuits
+    circuits.append(set([pt[0], pt[1]]))
+    return circuits
