bf462d14e86506e2fec6239d59df31e1f4f0a9c8
Baptiste Cottier <baptiste.cottier@inria.fr>
2025-12-08 22:26:36 +0100

    [2025][Python][07] add solution

--- events/year_2025/day_07/day_07.py
+++ events/year_2025/day_07/day_07.py
@@ -3,23 +3,47 @@ Advent of Code - Year 2025 - Day 7
 https://adventofcode.com/2025/day/7
 """
 
-
-def preprocessing(puzzle_input: str) -> list[str]:
-    """
-    Processes the puzzle input into a suitable data structure.
-    """
-    return puzzle_input.strip().split('\n')
+from collections import defaultdict
 
 
-def solver(data: list[str]) -> tuple[int, int]:
+def preprocessing(puzzle_input: str) -> tuple[set[tuple[int, int]], int, int]:
     """
-    Solves both parts of the puzzle.
-    Returns a tuple (part1_result, part2_result).
+    Parse puzzle input to extract splitter positions and starting coordinates.
     """
-    # TODO: Implement part 1
-    part1 = 0
-    
-    # TODO: Implement part 2  
-    part2 = 0
-    
-    return part1, part2
+    start = None
+    splitters = set()
+    for y, line in enumerate(puzzle_input.splitlines()):
+        for x, c in enumerate(line):
+            if c == '^':
+                splitters.add((x, y))
+            if c == 'S':
+                start = (x, y)
+    if start is None:
+        raise ValueError("No start found!")
+    return (splitters, *start)
+
+
+def solver(splitters, beam, y_start) -> tuple[int, int]:
+    """
+    Simulates beam splitting through splitters and returns the number of splits and final beam
+    count.
+    """
+    beams = {beam: 1}
+    n_splits = 0
+
+    for y in range(
+            y_start,
+            max(y for _, y in splitters) + 1,
+            2):
+        new_beams = defaultdict(int)
+
+        for x, n_beams in beams.items():
+            if (x, y) in splitters:
+                new_beams[x - 1] += n_beams
+                new_beams[x + 1] += n_beams
+                n_splits += 1
+            else:
+                new_beams[x] += beams[x]
+        beams = new_beams
+
+    return n_splits, sum(beams.values())
