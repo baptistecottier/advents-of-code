3dbc994406df72dc8454d2bedb6ab0cdd71cc40e
Baptiste Cottier <baptiste.cottier@inria.fr>
2025-12-09 23:29:47 +0100

    [2025][Python][09] add solution

--- events/year_2025/day_09/day_09.py
+++ events/year_2025/day_09/day_09.py
@@ -3,23 +3,55 @@ Advent of Code - Year 2025 - Day 9
 https://adventofcode.com/2025/day/9
 """
 
+from itertools import combinations
+from shapely import Polygon
+
 
 def preprocessing(puzzle_input: str) -> list[str]:
     """
-    Processes the puzzle input into a suitable data structure.
+    Parse puzzle input to extract coordinates and return them as a sorted list of tuples.
     """
-    return puzzle_input.strip().split('\n')
+    red_tiles = set()
+    for line in puzzle_input.splitlines():
+        (x, y) = tuple(map(int, line.split(',')))
+        red_tiles.add((x, y))
+    return sorted(red_tiles)
 
 
-def solver(data: list[str]) -> tuple[int, int]:
+def solver(red_tiles: list) -> tuple[int, int]:
     """
-    Solves both parts of the puzzle.
-    Returns a tuple (part1_result, part2_result).
+    Calculates the largest rectangle area that can be formed from a set of red tiles, 
+    distinguishing between rectangles fully contained within the main shape and those that are not.
     """
-    # TODO: Implement part 1
-    part1 = 0
-    
-    # TODO: Implement part 2  
-    part2 = 0
-    
-    return part1, part2
+    areas = {True: 0, False: 0}
+    geometries = []
+    to_connect = red_tiles.copy()
+
+    while to_connect:
+        pt_a = to_connect.pop()
+        coords = [pt_a]
+        found = True
+        while found:
+            found = False
+            for pt_b in to_connect:
+                if pt_a[0] == pt_b[0] or pt_a[1] == pt_b[1]:
+                    to_connect.remove(pt_b)
+                    coords.append(pt_b)
+                    pt_a = pt_b
+                    found = True
+                    break
+        geometries.append(Polygon(coords))
+
+    geometries = sorted(geometries, key = lambda g: g.area, reverse = True)
+    main_shape = geometries[0]
+
+    for hole in geometries[1:]:
+        main_shape = main_shape.difference(hole)
+
+    for ((xa, ya), (xb, yb)) in combinations(red_tiles, 2):
+        is_contained = main_shape.contains(Polygon(((xb, yb), (xa, yb), (xa, ya), (xb, ya))))
+        areas[is_contained] = max(
+            areas[is_contained],
+            (abs(xb - xa) + 1) * (1 + abs(yb - ya)))
+
+    return max(areas.values()), areas[True]
