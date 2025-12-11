7a9bcb5c30d2f8131ab1826d15253fa0b59f6a66
Baptiste Cottier <baptiste.cottier@icloud.com>
2025-12-11 21:55:09 +0100

    [2025][11] Python solution and puzzle input

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Solutions for [Advent of Code](https://adventofcode.com) in Python and Rust.
 
 **Year**      | **🐍** | **🦀** |
 :---:         | :---:  | :---:  |
-[2025](#2025) |  20    |   0    |
+[2025](#2025) |  22    |   0    |
 [2024](#2024) |  50    |  22    |
 [2023](#2023) |  31    |  12    |
 [2022](#2022) |  35    |  18    |
@@ -80,7 +80,7 @@ aocr 2024 1-5             # Run days 1-5
 | 08 | [Playground          ](events/year_2025/day_08/day_08.md)    | [⭐⭐](https://github.com/baptistecottier/advents-of-code/tree/main/events/year_2025/day_08/day_08.py)    |  |
 | 09 | [Movie Theater       ](events/year_2025/day_09/day_09.md)    | [⭐⭐](https://github.com/baptistecottier/advents-of-code/tree/main/events/year_2025/day_09/day_09.py)    |  |
 | 10 | [Factory             ](events/year_2025/day_10/day_10.md)    | [⭐⭐](https://github.com/baptistecottier/advents-of-code/tree/main/events/year_2025/day_10/day_10.py)    |  |
-| 11 | [TBD                 ](events/year_2025/day_11/day_11.md)    |                                                                                                          |  |
+| 11 | [Reactor             ](events/year_2025/day_11/day_11.md)    | [⭐⭐](https://github.com/baptistecottier/advents-of-code/tree/main/events/year_2025/day_11/day_11.py)    |  |
 | 12 | [TBD                 ](events/year_2025/day_12/day_12.md)    |                                                                                                          |  |
 
 </details>
--- events/year_2025/day_11/day_11.py
+++ events/year_2025/day_11/day_11.py
@@ -4,22 +4,89 @@ https://adventofcode.com/2025/day/11
 """
 
 
-def preprocessing(puzzle_input: str) -> list[str]:
-    """
-    Processes the puzzle input into a suitable data structure.
-    """
-    return puzzle_input.strip().split('\n')
+from collections import deque, defaultdict
 
 
-def solver(data: list[str]) -> tuple[int, int]:
+def preprocessing(puzzle_input: str) -> list[tuple[int, int]]:
     """
-    Solves both parts of the puzzle.
-    Returns a tuple (part1_result, part2_result).
+    Parse puzzle input to extract network connections and return them as a list of edges.
     """
-    # TODO: Implement part 1
-    part1 = 0
+    edges = []
+    for line in puzzle_input.splitlines():
+        ports = line.split()
+        for to in ports[1:]:
+            edges.append((ports[0][:-1], to))
+    return edges
+
+
+def solver(edge_list: list[tuple[int, int]]) -> tuple[int, int]:
+    """
+    Solves a graph path-counting problem by first performing topological sorting on the input edge
+    list. Calculates the number of paths from 'you' to 'out' for the first part of the solution.
+    For the second part, uses conditional logic to count paths between specific nodes ('fft',
+    'dac', 'svr', 'out'). 
     
-    # TODO: Implement part 2  
-    part2 = 0
+    As the graph is non-cyclic, if no direct path exists from 'fft' to 'dac', it means the path to
+    follow is 'svr' -> 'dac' -> 'fft' -> 'out'. Otherwise, it multiplies path counts from 'svr' to
+    'fft' and from 'dac' to 'out'.
     
-    return part1, part2
+    Returns both results as a tuple of integers.
+    """
+    sorted_graph = topological_sort(edge_list)
+    n_paths_from_you_to_out = count_paths(sorted_graph, 'you', 'out')
+    n_paths = count_paths(sorted_graph, 'fft', 'dac')
+
+    if n_paths == 0:
+        n_paths += count_paths(sorted_graph, 'svr', 'dac')
+        n_paths *= count_paths(sorted_graph, 'dac', 'fft')
+        n_paths *= count_paths(sorted_graph, 'fft', 'out')
+    else:
+        n_paths *= count_paths(sorted_graph, 'svr', 'fft')
+        n_paths *= count_paths(sorted_graph, 'dac', 'out')
+
+    return n_paths_from_you_to_out, n_paths
+
+
+def topological_sort(edge_list: list[tuple[int, int]]):
+    """
+    Performs topological sorting on a directed graph using Kahn's algorithm.
+    """
+    ports = set([u for u, _ in edge_list] + ['out'])
+    graph = defaultdict(list)
+    indegree = {p: 0 for p in ports}
+
+    for u, v in edge_list:
+        print(indegree)
+        graph[u].append(v)
+        indegree[v] += 1
+
+    queue = deque()
+    for i in ports:
+        if indegree[i] == 0:
+            queue.append(i)
+
+    topo_order = []
+    while queue:
+        node = queue.popleft()
+        topo_order.append((node, graph[node]))
+
+        for neighbor in graph[node]:
+            if indegree[neighbor] == 1:
+                queue.append(neighbor)
+            indegree[neighbor] -= 1
+
+    return topo_order
+
+
+def count_paths(topo_order, source, destination):
+    """
+    Count the number of paths from source to destination in a topologically ordered DAG.
+    """
+    ways = defaultdict(int)
+    ways[source] = 1
+
+    for node, neighbors in topo_order:
+        for neighbor in neighbors:
+            ways[neighbor] += ways[node]
+
+    return ways[destination]
